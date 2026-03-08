a670c5a1605a3ad80f8d88896b3f4a8b12442b9d
EgorovaVarvara <408575@niuitmo.ru>
2026-03-08 22:14:22 +0300

    Arcsin realisation

---
+++ task1/src/main/java/org/example/Arcsin.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Arcsin {
+    // допустим у нас ряд маклорена, а не тейлора
+    public static double count(double x, int n){
+        if (Math.abs(x) > 1) {
+            return Double.NaN;
+        }
+
+        // если аргумент 0 - то возвращаем 0
+        if (x == 0.0 || Double.isNaN(x)) {
+            return x;
+        }
+
+        // при х близких к 1 ряд медленно сходится, поэтому делаем преобразование аргумента и считаем ряд с меньшим аргументом
+        if (Math.abs(x) > 0.8){
+            return Math.PI / 2 - arcsinSeries(Math.sqrt(1 - x*x), n);
+        }
+        return arcsinSeries(x, n);
+    }
+
+    // функция для вычисления ряда
+    // каждый член ряда считается рекуррентно
+    private static double arcsinSeries(double x, int n){
+        double sum = x;
+        double x_curr = x;
+
+        for (int i = 1; i < n; i++){
+            double x_new = x_curr * (Math.pow(2 * i - 1, 2))/((2 * i + 2)*(2 * i + 3)) * x * x;
+            sum += x_new;
+            x_curr = x_new;
+        }
+        return sum;
+    }
+}
--- task1/src/main/java/org/example/Main.java
+++ task1/src/main/java/org/example/Main.java
@@ -2,10 +2,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        if (args.length != 2){
+            System.out.println("Usage: java App <x> <n>");
+        }else{
+            try{
+                double x = Double.parseDouble(args[0]);
+                int n = Integer.parseInt(args[1]);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+                double result = Arcsin.count(x, n);
+
+                if (Double.isNaN(result)) {
+                    System.out.println("Error: x must be in [-1...1].");
+                    return;
+                }
+
+                System.out.printf("arcsin(%.2f) = %.6f%n (with %d terms)", x, result, n);
+            }catch(NumberFormatException e){
+                System.out.println("Invalid input. Please enter a valid number for x and an integer for n.");
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+
         }
+        // провалидировать, распарсить
+
+        // аргументы: x - аргумент функции, n - до какого порядка раскладывать в ряд
+
+        // посчитать значение ряда (вынести в отдельный класс)
+
+        // вывести красиво результат
     }
 }
